c845d3d579cb85ffb79f5185ec7789c86fd78745
Heini Ahokas <ahokas.heini@gmail.com>
2020-03-01 19:34:08 +0100

    Set up UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Icon from '@material-ui/core/Icon';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [searchInput, setSearchInput] = useState('');
+
+	const handleChange = event => {
+		setSearchInput(event.target.value);
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (searchInput) {
+
+		}
+	};
+
+	return (
+			<Container maxWidth='sm'>
+				<AppBar elevation={0} position='static'>
+					<Toolbar>
+						<Typography variant='h6'>
+							Searching for Javascript
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Paper component="main" elevation={0}>
+					<form onSubmit={handleSubmit}>
+						<FormControl fullWidth>
+							<TextField
+								onChange={handleChange}
+								id="input-with-icon-textfield"
+								value={searchInput}
+								label="Type your query and hit enter"
+								InputProps={{
+									endAdornment: (
+										<InputAdornment position="end">
+											<Icon>search</Icon>
+										</InputAdornment>
+									),
+								}}
+							/>
+						</FormControl>
+					</form>
+				</Paper>
+			</Container>
+	);
 }
 
 export default App;
